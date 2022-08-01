c9ea71052efdb0b63463c059521d81ce26c1f39f
Alexander Sudalin <sudalin@MacBook-Air-Alexander.local>
2022-08-01 16:05:03 +0300

    fix

--- internal/pkg/service/Messageservice.go
+++ internal/pkg/service/Messageservice.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"log"
+
 	"github.com/sudak-91/pc_bot/internal/pkg/server"
 	types "github.com/sudak-91/telegrambotgo/TelegramAPI/Types"
 )
@@ -8,12 +10,12 @@ import (
 //TODO: Обработка ошибок
 func (t *TelegramUpdater) messageService(Message types.TelegramMessage) ([]byte, error) {
 	switch server.Util.Stage[Message.From.ID] {
-
 	case 10:
 		return t.Execute("/addnews", Message)
 	case 20:
 		return t.Execute("/addquestion", Message)
 	default:
+		log.Println("default message")
 		return t.Routing(Message)
 
 	}
@@ -23,10 +25,12 @@ func (t *TelegramUpdater) Routing(Message types.TelegramMessage) ([]byte, error)
 	for _, ent := range Message.Entities {
 		switch ent.Type {
 		case "bot_command":
+			log.Println("bot_command")
 			return t.Execute(Message.Text, Message)
 		default:
-			return nil, nil
+			continue
 		}
 	}
+	log.Println("no entity")
 	return nil, nil
 }
