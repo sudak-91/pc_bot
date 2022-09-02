74640da3e5cc1100041c9a5ec3d6a6c3fa5b4e10
Alexander Sudalin <sudalin@MacBook-Air-Alexander.local>
2022-09-02 08:19:47 +0300

    Add AddNewManual command logic

---
+++ internal/pkg/command/addnewmanual.go
@@ -0,0 +1,27 @@
+package command
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/sudak-91/pc_bot/internal/pkg/server"
+	"github.com/sudak-91/pc_bot/internal/pkg/service"
+	types "github.com/sudak-91/telegrambotgo/TelegramAPI/Types"
+)
+
+type AddNewManual struct {
+}
+
+func (this *AddNewManual) Handl(data interface{}) ([]byte, error) {
+	msg, ok := data.(types.TelegramCallbackQuery)
+	if !ok {
+		log.Print("AddNewManual Error")
+		return nil, fmt.Errorf("Dont have, telegram query")
+	}
+	var Answer types.TelegramSendMessage
+	Answer.Method = "sendMessage"
+	Answer.Text = "Введите название фирмы и модель к которой будет загружен мануал"
+	server.Util.Stage[msg.From.ID] = service.Addmanualinfo
+	return json.Marshal(Answer)
+}
