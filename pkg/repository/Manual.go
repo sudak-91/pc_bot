86178cc6510ba8fc4287e2fb83d7b5bf921f1f13
Alexander Sudalin <sudalin@MacBook-Air-Alexander.local>
2022-09-26 21:49:27 +0300

    fix

--- pkg/repository/Manual.go
+++ pkg/repository/Manual.go
@@ -5,7 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Manual struct {
 	ManualID    primitive.ObjectID `bson:"_id"`
 	FileUniqID  string             `bson:"fileid"`
-	Firm        *Firm              `bson:"firm"`
+	Firm        Firm               `bson:"firm"`
 	DeviceModel string             `bson:"device"`
 	Contributer int64              `bosn:"userid"`
 	Version     string             `bson:"version,omitempty"`
