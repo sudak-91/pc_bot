98270d4bc94cd36936e233ea1766e303e3a85b55
Alexander Sudalin <sudalin@MacBook-Air-Alexander.local>
2022-09-26 19:36:01 +0300

    fix

--- internal/pkg/notificator/notificator.go
+++ internal/pkg/notificator/notificator.go
@@ -1,6 +1,7 @@
 package notificator
 
 import (
+	"encoding/base64"
 	"fmt"
 	"log"
 	"os"
@@ -72,8 +73,8 @@ func sendAddFirmNotification(firm pubrep.Firm) {
 func editFirmNotioficationKeyboard(firm pubrep.Firm) tgtype.TelegramInlineKeyboardMarkup {
 	keyboard := &keyboardmaker.InlineCommandKeyboard{}
 	keyboard.MakeGrid(2, 1)
-	editCommandCallbackString := fmt.Sprintf("/editfirm %s", firm.ID.String())
-	confirmCommandCallbackString := fmt.Sprintf("/approvedfirm %s", firm.ID.String())
+	editCommandCallbackString := fmt.Sprintf("/editfirm %s", base64.RawURLEncoding.EncodeToString(firm.ID[:]))
+	confirmCommandCallbackString := fmt.Sprintf("/approvedfirm %s", base64.RawURLEncoding.EncodeToString(firm.ID[:]))
 	keyboard.AddButton("Редактировать название", editCommandCallbackString, 0, 0)
 	keyboard.AddButton("Утвердить название фирмы", confirmCommandCallbackString, 1, 0)
 	return keyboard.GetKeyboard()
