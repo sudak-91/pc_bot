cc469c8a327e2ed6fe93dc79e66a0a4abff6d306
Alexander Sudalin <sudalin@MacBook-Air-Alexander.local>
2022-10-08 20:37:10 +0300

    Fix

--- internal/pkg/command/allfirmslist.go
+++ internal/pkg/command/allfirmslist.go
@@ -43,6 +43,7 @@ func (a *AllFirmsList) Handl(data interface{}) ([]byte, error) {
 	EditMessage.ChatID = fmt.Sprintf("%s", query.From.ID)
 	EditMessage.MessageID = int(query.Message.MessageID)
 	EditMessage.ReplyMarkup = keyboard
+	EditMessage.Text = "Выберите фирму, чтобы получить список устройств, на которые имеются руководства"
 	err = methods.EditMessageTextMethod(EditMessage, os.Getenv("BOT_KEY"))
 	if err != nil {
 		return util.CommandErrorHandler(&answer, err)
